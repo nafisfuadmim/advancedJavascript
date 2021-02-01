22a433bac826931c7ec39ea0a7843e35267c807d
Nafis Fuad Mim <nafisnfm@.com>
2021-02-02 01:50:44 +0600

    add' git commit -madd git commit -m"add"
    git push
    
    node map-filter.js
    node map-filter.js
    
    git commit -m"add

---
+++ map-filter.js
@@ -0,0 +1,19 @@
+const numbers = [3, 4, 5, 6, 7, 8, 9,];
+// const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+
+    
+// }
+
+// const result = numbers.map(x => x * x);
+
+
+const bigger = numbers.filter(x => x < 5);
+
+
+const isThere = numbers.find(x => x > 5);
+
+console.log(isThere);
---
+++ true-false.js
@@ -0,0 +1,7 @@
+const age = 4;
+if(age >0){
+    console.log("kotha sotto");
+}
+else{
+    console.log("kotha Mittha")
+}
