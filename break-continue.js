29f31802875394b6869216e73388f85edacfebea
Nafis Fuad Mim <nafisnfm@.com>
2021-02-02 23:17:21 +0600

    add

---
+++ break-continue.js
@@ -0,0 +1,12 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let i = 0; i < num.length; i++) {
+    const element = num[i];
+    
+    
+    if (i >3) {
+        break;
+        
+    }
+    
+    console.log(element);
+}
